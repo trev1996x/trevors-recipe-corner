192f1cfffa2e0ba03cfe46545d67f47330474fcd
TrevorLeamon <trev1996x@gmail.com>
2025-07-12 13:14:43 -0500

    update _

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 
     // Load header and footer
-    loadHTML('header.html', 'header-placeholder');
-    loadHTML('footer.html', 'footer-placeholder');
+    loadHTML('_header.html', 'header-placeholder');
+    loadHTML('_footer.html', 'footer-placeholder');
 });
