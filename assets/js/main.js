26f0b4e394e38f603d51a7c1ae327dad79b53b98
TrevorLeamon <trev1996x@gmail.com>
2025-07-13 12:08:56 -0500

    update header css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,39 @@
+// /assets/js/main.js
+
 document.addEventListener("DOMContentLoaded", function() {
     // Function to fetch and insert HTML content
     const loadHTML = (filePath, elementId) => {
-        fetch(filePath)
-            .then(response => response.text())
+        return fetch(filePath) // Return the fetch promise
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.text();
+            })
             .then(data => {
                 document.getElementById(elementId).innerHTML = data;
             })
             .catch(error => console.error('Error loading HTML:', error));
     };
 
-    // Corrected paths starting from the root (/)
-    loadHTML('/assets/_header.html', 'header-placeholder');
-    loadHTML('/assets/_footer.html', 'footer-placeholder');
+    // Function to set the active navigation link
+    const setActiveLink = () => {
+        const navLinks = document.querySelectorAll('.site-header nav a');
+        const currentPageUrl = window.location.pathname;
+
+        navLinks.forEach(link => {
+            // Check if the link's href matches the current page's URL
+            if (link.getAttribute('href') === currentPageUrl) {
+                link.classList.add('active-link');
+            }
+        });
+    };
+
+    // Load header and footer, then set the active link
+    Promise.all([
+        loadHTML('/assets/_header.html', 'header-placeholder'),
+        loadHTML('/assets/_footer.html', 'footer-placeholder')
+    ]).then(() => {
+        setActiveLink(); // Run this function after the header has been loaded
+    });
 });
