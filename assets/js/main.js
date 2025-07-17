a5d942b6cc01be5d14cfddc4fbdb544cdf8ab634
TrevorLeamon <trev1996x@gmail.com>
2025-07-16 23:10:45 -0500

    removed firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAINk6KXEIpRc-xkGJtseTLGb5LfMQfnNU",
-  authDomain: "trevors-recipe-app.firebaseapp.com",
-  projectId: "trevors-recipe-app",
-  storageBucket: "trevors-recipe-app.firebasestorage.app",
-  messagingSenderId: "287120956802",
-  appId: "1:287120956802:web:b4089373b9181a1828439b",
-  measurementId: "G-Z7PW9J7ZVW"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 // /assets/js/main.js
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -69,23 +47,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-
-// Check if we are on the login page
-if (document.getElementById('login-form')) {
-    const loginForm = document.getElementById('login-form');
-    loginForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        const errorMessage = document.getElementById('error-message');
-
-        auth.signInWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                // Login successful, redirect to a new admin page
-                window.location.href = '/admin.html';
-            })
-            .catch((error) => {
-                errorMessage.textContent = error.message;
-            });
-    });
-}
